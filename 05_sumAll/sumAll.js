0405b8bc6485f3531bd611fd08c619b795e8d495
Thiago <thiagolemos@id.uff.br>
2022-08-15 11:18:43 -0300

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
 
+    let result = 0;
+
+    if (int1 > int2) [int1, int2] = [int2, int1];
+
+    if ((int1 || int2) < 0) return "ERROR";
+
+    if ((!Number.isInteger(int1) || !Number.isInteger(int2))) return "ERROR";
+
+    for (let i = int1; i <= int2; i++) result += i;
+
+    return result;
 };
 
 // Do not edit below this line

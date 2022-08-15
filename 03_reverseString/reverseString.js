6229897c1d482ea0d6951760f028f2aa082d4d07
Thiago <thiagolemos@id.uff.br>
2022-08-15 10:43:26 -0300

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let newWord = "";
+    let oldWord = word;
+    let length = oldWord.length;
 
+    for (let i = 0; i < length; i++){
+        newWord += oldWord.slice(-1);
+        oldWord = oldWord.slice(0, -1);
+    }
+    
+    return newWord;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;

faf0537d4a527b664b7a3c8f86d7553e2ee6e4df
Thiago <thiagolemos@id.uff.br>
2022-08-15 10:29:02 -0300

    Finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    if (num < 0) return "ERROR";
+    let finalStr = "";
+    for (let i = 0; i < num; i++){
+        finalStr += string;
+    }
+
+    return finalStr;
 };
 
 // Do not edit below this line
